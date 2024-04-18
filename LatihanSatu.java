dcaeea78d9240d457670a41b04b93dc2d7ecfee5
baellia <elsyaliaa@gmail.com>
2024-04-18 16:43:52 +0700

    Praktikum 6

---
+++ LatihanSatu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+/**
+ *
+ * @author elsya
+ */
+public class LatihanSatu {
+    
+    public static void main(String[] args){//agar dapat di run sebagai class utama
+        JFrame myWindow;
+        
+        myWindow = new JFrame();//pembuatan object
+        myWindow.setSize(700, 1000);//setting ukuran form
+        myWindow.setTitle("My First Java Program");//judul untuk form
+        myWindow.setVisible(true);//untuk menampilkan form
+    }
+}
